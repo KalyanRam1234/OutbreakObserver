253a70e5d24982d87dff4d7def93b58f57cd11f6
Jashwanth-Kadaru <jashwanthkadaru2021@gmail.com>
2023-04-23 00:50:47 +0530

    Modified Quarantine DAO. Added methods.

--- src/Quarantine/Quarantine.java
+++ src/Quarantine/Quarantine.java
@@ -3,21 +3,25 @@ package Quarantine;
 public class Quarantine {
     private String caseId;
     private String startDate, healthStatus;
+    private String endDate;
     private String duration;
     
     public Quarantine( ) {}
 
-    public Quarantine(String caseId, String startDate, String healthStatus, String duration){
+    public Quarantine(String caseId, String startDate, String healthStatus, String duration, String endDate){
         this.caseId=caseId;
         this.startDate=startDate;
         this.healthStatus=healthStatus;
         this.duration=duration;
+        this.endDate=endDate;
     }
 
     public String getcaseId() { return caseId; }
-    public String getDate() { return startDate; }
+    public String getStartDate() { return startDate; }
 
     public String getStatus() { return healthStatus; }
+
+    public String getEndDate() {return endDate;}
 
     public String getDuration() { return duration;}
 
--- src/Quarantine/QuarantineDAO.java
+++ src/Quarantine/QuarantineDAO.java
@@ -1,5 +1,10 @@
 package Quarantine;
 
+import java.util.ArrayList;
+
 public interface QuarantineDAO {
-    
+    public Quarantine getCaseDetails(String caseid);
+    public ArrayList<Quarantine> getQuarantineByStartDate(String startDate);
+    public ArrayList<Quarantine> getQuarantineByEndDate(String endDate);  
+    public void enterQuarantineCase(String caseId, String startDate, String healthStatus, String duration, String endDate); 
 }
