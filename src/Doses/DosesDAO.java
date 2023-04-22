4c8dfb639322faf32b7a3cc36edfa52e6d4b46a4
Jashwanth-Kadaru <jashwanthkadaru2021@gmail.com>
2023-04-23 01:13:24 +0530

    Modified Dosees DAO. enter, delete, get methods added.

--- src/Doses/DosesDAO.java
+++ src/Doses/DosesDAO.java
@@ -1,5 +1,7 @@
 package Doses;
 
 public interface DosesDAO {
-    
+    public void enterDose(String studentId, int doseNo, String dateTaken, String vaccineName);
+    public void deleteDose(String studentId, int doseNo);
+    public Doses getDose(String studentId, int doseNo); 
 }
