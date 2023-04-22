2c54a2a97e977c834acd9a07644998c6c47f45ba
Jashwanth-Kadaru <jashwanthkadaru2021@gmail.com>
2023-04-22 15:51:54 +0530

    modified rtpcr

--- src/RTPCR/RTPCRDAO.java
+++ src/RTPCR/RTPCRDAO.java
@@ -1,5 +1,9 @@
 package RTPCR;
 
 public interface RTPCRDAO {
-    
+    public RTPCR getRTPCRByStudentId(String id);
+    public RTPCR getRTPCRByStudentId_Date(String id, String testDate);
+    public RTPCR getRTPCR_postive();
+    public RTPCR getRTPCRByStudentId_LastDate(String id);
+    // public Student getStudentsByBatch(String batch);
 }
--- src/RTPCR/RTPCRDAO_JDBC.java
+++ src/RTPCR/RTPCRDAO_JDBC.java
@@ -7,4 +7,42 @@ public class RTPCRDAO_JDBC implements RTPCRDAO{
     public RTPCRDAO_JDBC(Connection dbconn){
         dbConnection=dbconn;
     }
+
+    @Override
+    public RTPCR getRTPCRByStudentId(String id){
+        
+        RTPCR rtpcr=new RTPCR();
+        String sql;
+		Statement stmt = null;
+        
+        try{
+            stmt=dbConnection.createStatement();
+            sql="select * from rtpcr where studentId = " + id;
+            ResultSet rs= stmt.executeQuery(sql);
+
+
+        }
+        catch ( SQLException ex){
+            System.out.println("SQLException: " + ex.getMessage());
+		    System.out.println("SQLState: " + ex.getSQLState());
+		    System.out.println("VendorError: " + ex.getErrorCode());
+        }
+        return rtpcr;
+    }
+    
+    @Override
+    public RTPCR getRTPCRByStudentId_Date(String id, String testDate){
+        return null;
+    }
+    
+    @Override
+    public RTPCR getRTPCR_postive(){
+        return null;
+    }
+
+    @Override
+    public RTPCR getRTPCRByStudentId_LastDate(String id){
+        return null;
+    }
+
 }
