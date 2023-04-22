1a5b8f628234cc00240e6a5c0ef51eaa45d2a8a5
Kalyan Ram <kalyanram@pop-os.localdomain>
2023-04-22 15:47:02 +0530

    First usecase done

--- src/DAO_Main.java
+++ src/DAO_Main.java
@@ -1,3 +1,26 @@
+import Student.*;
+import UseCases.UseCase1.*;
+import java.util.ArrayList;
 public class DAO_Main {
+    public static DAO_Factory daoFactory;
+    public static void main(String[] args){
+        ArrayList<useCase1> s1;
+        try{
+            daoFactory = new DAO_Factory();
+            daoFactory.activateConnection();
+            StudentDAO dao=daoFactory.getStudentDAO();
+            System.out.println(dao.getStudentCount());
+            useCase1_DAO sdao = daoFactory.getuseCase1DAO();
+            s1=sdao.getCollegeRTPCR_Status("2023-04-12" );
+
+            System.out.println(s1.size());
+            daoFactory.deactivateConnection(DAO_Factory.TXN_STATUS.COMMIT);
+
+        }catch(Exception e){
+                //Handle errors for Class.forName
+                daoFactory.deactivateConnection( DAO_Factory.TXN_STATUS.ROLLBACK );
+                e.printStackTrace();
+        }
+    } 
     
 }
--- src/Student/Student.java
+++ src/Student/Student.java
@@ -3,18 +3,18 @@ package Student;
 public class Student {
     private String id,fname,lname, email_id;
     private String gender;
-    private String batch, date;
+    private String  date;
     private String branch, roomno;
 
     public Student(){}
 
-    public Student(String id, String fname, String lname, String date, String email_id, String gender, String batch, String branch, String roomno){
+    public Student(String id, String fname, String lname, String date, String email_id, String gender, String branch, String roomno){
         this.id=id;
         this.fname=fname;
         this.lname=lname;
         this.email_id=email_id;
         this.gender=gender;
-        this.batch=batch;
+        this.date=date;
         this.branch=branch;
         this.roomno=roomno;
     }
@@ -23,8 +23,8 @@ public class Student {
     public String getfname(){ return fname;}
     public String getlname() { return lname; }
     public String getemail() { return email_id; }
+    public String getDate() { return date; }
     public String getgender() { return gender; }
-    public String getbatch() { return batch; }
     public String getbranch() { return branch; }
     public String getroomno() { return roomno; }
 }
