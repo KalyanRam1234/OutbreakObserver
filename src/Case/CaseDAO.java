5995d513d82b0e055fb1a208f271f3c0387b18e8
Jashwanth-Kadaru <jashwanthkadaru2021@gmail.com>
2023-04-23 01:34:53 +0530

    Modified CaseDAO.java file. Added create, delete, getbysid, getbycid methods.

--- src/Case/CaseDAO.java
+++ src/Case/CaseDAO.java
@@ -1,5 +1,8 @@
 package Case;
 
 public interface CaseDAO {
-    
+    public void enterNewCase(String caseId, String studentId, String qroomNo, String testId , String diagnosisDate);
+    public void deleteCase(String caseId, String studentId);
+    public Case getCaseByCaseId(String caseId);
+    public Case getCaseByStudentId(String studentId); 
 }
