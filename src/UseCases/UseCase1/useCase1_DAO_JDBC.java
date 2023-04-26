2c240508e4b3e92dd9f3d7ff2742912b98f42d00
Jashwanth-Kadaru <jashwanthkadaru2021@gmail.com>
2023-04-26 22:34:09 +0530

    Tested UseCase1 with UI.

--- src/UseCases/UseCase1/useCase1_DAO_JDBC.java
+++ src/UseCases/UseCase1/useCase1_DAO_JDBC.java
@@ -61,8 +61,8 @@ public class useCase1_DAO_JDBC implements useCase1_DAO{
     }
 
     @Override
-    public useCase1 getStudentRTPCR_Status(String rollNo){
-        useCase1 s=new useCase1();
+    public ArrayList<useCase1> getStudentRTPCR_Status(String rollNo){
+        ArrayList<useCase1> list=new ArrayList<useCase1>();
         String sql;
 		Statement stmt = null;
 
@@ -72,8 +72,9 @@ public class useCase1_DAO_JDBC implements useCase1_DAO{
             ResultSet rs= stmt.executeQuery(sql);
 				//Retrieve by column name
             while(rs.next()){
+                useCase1 s =new useCase1();
                 s=getInfo(rs);
-                break;
+                list.add(s);
             }
         }
         catch ( SQLException ex){
@@ -81,18 +82,18 @@ public class useCase1_DAO_JDBC implements useCase1_DAO{
 		    System.out.println("SQLState: " + ex.getSQLState());
 		    System.out.println("VendorError: " + ex.getErrorCode());
         }
-        return s;
+        return list;
     }
 
     @Override
-    public ArrayList<useCase1> getBatchRTPCR_Status(String batch, String Date){
+    public ArrayList<useCase1> getBatchRTPCR_Status(String batch){
         ArrayList<useCase1> list=new ArrayList<useCase1>(0);
         String sql;
 		Statement stmt = null;
 
         try{
             stmt=dbConnection.createStatement();
-            sql="select s.studentId, concat(fname,' ',lname) as " + "Full_Name" + ", s.gender, s.roomNo, s.emailId, r.testId,r.testDate, r.test_result from student as s join rtpcr as r where s.studentId like '%" + batch+ "%' and s.studentId=r.studentId and r.testDate='"+ Date +"'" ;
+            sql="select s.studentId, concat(fname,' ',lname) as " + "Full_Name" + ", s.gender, s.roomNo, s.emailId, r.testId,r.testDate, r.test_result from student as s join rtpcr as r where s.studentId like '%" + batch+ "%' and s.studentId=r.studentId";// and r.testDate='"+ Date +"'" ;
 
             ResultSet rs= stmt.executeQuery(sql);
             
@@ -136,5 +137,36 @@ public class useCase1_DAO_JDBC implements useCase1_DAO{
         return list;
     }
 
+    public void enterIntoRTPCRTable(String studentId, String testId, String testDate, String testResult) {
+        PreparedStatement preparedStatement = null;																																																																																																																																													
+		String sql;
+		sql = "insert into rtpcr(studentId, testId, testDate, test_result, certif) values (?,?,?,?,NULL)";
 
+		try {
+			preparedStatement = dbConnection.prepareStatement(sql);
+ 
+            int tr=0;
+            if(testResult.equals("Postive")) tr=1;
+            if(testResult.equals("Negative")) tr=0;
+			preparedStatement.setString(1, studentId);
+            preparedStatement.setString(2, testId);
+			preparedStatement.setString(3, testDate);
+            preparedStatement.setInt(4, tr);
+ 
+			// execute insert SQL stetement
+			preparedStatement.executeUpdate();
+
+            System.out.println("Data was entered successfully.\n");
+		} catch (SQLException e) {
+ 			System.out.println(e.getMessage());
+ 		}
+
+		try{
+			if (preparedStatement != null) {
+				preparedStatement.close();
+			}
+		} catch (SQLException e) {
+ 			System.out.println(e.getMessage());
+ 		}
+    }
 }
