d370153441c488a6cb31bd1c0f1a2f83ac9a2f55
Jashwanth-Kadaru <jashwanthkadaru2021@gmail.com>
2023-04-26 22:40:18 +0530

    Modified create.sql file.

--- src/sql/OO_create.sql
+++ src/sql/OO_create.sql
@@ -58,7 +58,7 @@ create table dose(
     doseNo int,
     dateTaken Date,
     vaccineName varchar(15),
-    constraint pk_dose PRIMARY KEY(studentId,dateTaken,vaccineName)
+    constraint pk_dose PRIMARY KEY(studentId,doseNo)
 );
 
 create table login_t(
